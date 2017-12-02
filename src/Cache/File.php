029c8da4d06230f3419fa4ac0aa4052d58b4bc4c
locphp <hellophp@qq.com>
2017-12-02 22:17:50 +0800

    -

---
+++ src/Cache/File.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Rsf\Cache;
+
+class File {
+
+    use \Rsf\Traits\Singleton;
+
+    public $enable = false;
+
+    /**
+     * @return $this
+     * @throws \Exception
+     */
+    public function init() {
+        if (!is_dir(getini('data/_cache'))) {
+            throw new \Exception('路径:' . getini('data/_cache') . ' 不可写');
+        }
+        $this->enable = true;
+        return $this;
+    }
+
+    public function close() {
+
+    }
+
+    /**
+     * @param $key
+     * @return null
+     */
+    public function get($key) {
+        $cachefile = getini('data/_cache') . $key . '.php';
+        if (is_file($cachefile)) {
+            $data = include $cachefile;
+            if ($data && ($data['timeout'] == 0 || $data['timeout'] > time())) {
+                return $data['data'];
+            }
+            unlink($cachefile);
+        }
+        return null;
+    }
+
+    /**
+     * @param $key
+     * @param $val
+     * @param int $ttl
+     * @return bool|int
+     */
+    public function set($key, $val, $ttl = 0) {
+        if ($ttl > 0) {
+            $timeout = time() + $ttl;
+        } else {
+            //默认存储永久
+            $timeout = 0;
+        }
+
+        $cachefile = getini('data/_cache') . $key . '.php';
+        $cachedata = "return array('data' => '{$val}', 'timeout' => {$timeout});";
+        $content = "<?php \n//CACHE FILE, DO NOT MODIFY ME PLEASE!\n//Identify: " . md5($key . time()) . "\n\n{$cachedata}";
+        return $this->save($cachefile, $content, FILE_WRITE_MODE);
+    }
+
+    /**
+     * @param $key
+     * @param int $ttl
+     * @return bool
+     */
+    public function expire($key, $ttl = 0) {
+        return false;
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function rm($key) {
+        $cachefile = getini('data/_cache') . $key . '.php';
+        if (file_exists($cachefile)) {
+            unlink($cachefile);
+        }
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function clear() {
+        $cachedir = getini('data/_cache');
+        $files = \Rsf\Helper\File::list_files($cachedir);
+        foreach ($files as $file) {
+            unlink($cachedir . $file);
+        }
+        return true;
+    }
+
+    /**
+     * @param $filename
+     * @param $content
+     * @param $mode
+     * @return bool|int
+     */
+    public function save($filename, $content, $mode) {
+        if (!is_file($filename)) {
+            file_exists($filename) && unlink($filename);
+            touch($filename) && chmod($filename, FILE_WRITE_MODE); //全读写
+        }
+        $ret = file_put_contents($filename, $content, LOCK_EX);
+        if ($ret && FILE_WRITE_MODE != $mode) {
+            chmod($filename, $mode);
+        }
+        return $ret;
+    }
+
+}
---
+++ src/Cache/Memcache.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Rsf\Cache;
+
+class Memcache {
+
+    use \Rsf\Traits\Singleton;
+
+    public $enable = false;
+    private $_link = null;
+    private $_plink = false;
+
+    public function __destruct() {
+        $this->close();
+    }
+
+    /**
+     * @param $config
+     * @return $this
+     * @throws \Exception
+     */
+    public function init($config) {
+        try {
+            $this->_link = new \Memcache();
+            if ($config['pconnect']) {
+                $this->_plink = true;
+                $server = 'pconnect';
+            } else {
+                $server = 'connect';
+            }
+            $connect = $this->_link->$server($config['host'], $config['port'], $config['timeout']);
+            if ($connect) {
+                $this->enable = true;
+            }
+        } catch (\MemcachedException $e) {
+            throw new \Exception('memcache初始化错误');
+        }
+        return $this;
+    }
+
+    public function close() {
+        if (!$this->_plink) {
+            $this->_link && $this->_link->close();
+        }
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function get($key) {
+        try {
+            return $this->_link->get($key);
+        } catch (\MemcachedException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @param int $ttl
+     * @return bool
+     */
+    public function set($key, $value, $ttl = 0) {
+        try {
+            $data = $this->get($key);
+            if ($data) {
+                return $this->_link->set($key, $value, MEMCACHE_COMPRESSED, $ttl);
+            } else {
+                return $this->_link->add($key, $value, MEMCACHE_COMPRESSED, $ttl);
+            }
+        } catch (\MemcachedException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param $key
+     * @param int $ttl
+     * @return bool
+     */
+    public function expire($key, $ttl = 0) {
+        return false;
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function rm($key) {
+        try {
+            return $this->_link->delete($key);
+        } catch (\MemcachedException $e) {
+            return false;
+        }
+    }
+
+    public function clear() {
+        return $this->_link->flush();
+    }
+
+}
